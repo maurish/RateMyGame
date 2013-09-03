e2b84575b89b80e8339c1252006c2211cf2d7db4
Pauli Perälä <perala.pauli@gmail.com>
2013-09-03 15:02:07 +0300

    init Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RateMyGame::Application.config.secret_key_base = '6fe121851041407b3d052c2d24aac923ad9fba5af7b646d301c745645a6f26b177980933ec0e40a24b4bde34d94858df441d86718e8a606a453030826d8464be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RateMyGame::Application.config.session_store :cookie_store, key: '_RateMyGame_session'
