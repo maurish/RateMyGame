5e59eca730d63078c445d7910abc922e77b48093
Pauli Perälä <perala.pauli@gmail.com>
2013-09-21 16:56:55 +0300

    some helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?, :currently_logged_as?
+
+  def currently_logged_as? user
+  	user == current_user
+  end
+
+  def current_user
+  	id = session[:user_id]
+  	return nil if id.nil?
+  	User.find id
+  end
+
+  def logged_in?
+  	not current_user.nil?
+  end
 end
