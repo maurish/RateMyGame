0f9e96155ceaa42a21c9a7e0c810e6807554bce4
Pauli Perälä <perala.pauli@gmail.com>
2013-09-22 02:31:37 +0300

    some basic validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true, uniqueness: true
+  validates :name, 	presence: true, 
+  					uniqueness: true
   has_secure_password
+  validates :password, 	format:{with: /\w*/, message:"Must be contained of letters and numbers"},
+  						length:{:in => 6..36}
 
   def to_s
   	"#{name}"
