b4345393d7fc394468047d94b4aab9cf5d9320bc
Pauli Perälä <perala.pauli@gmail.com>
2013-09-23 02:26:20 +0300

    extended seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,30 @@ bullfrog = Publisher.create name: "Bullfrog Productions", year: 1987, city: "Gui
 
 pauli = User.create name: "pauli", password:"iluap", password_confirmation: "iluap"
 paula = User.create name: "paula", password:"aluap", password_confirmation: "aluap"
+admin = User.create name: "admin", password:"password", password_confirmation:"password"
 
-warcraft 	= blizzard.games.create name: "Warcraft"
-warcraft2 	= blizzard.games.create name: "Warcraft II"
-diablo 		= blizzard.games.create name: "Diablo"
-starcraft 	= blizzard.games.create name: "Starcraft"
-diablo2 	= blizzard.games.create name: "Diablo II"
-warcraft3	= blizzard.games.create name: "Warcraft III"
-wow 		= blizzard.games.create name: "World of Warcraft"
-starcraft2	= blizzard.games.create name: "Starcraft II"
-diablo3 	= blizzard.games.create name: "Diablo III"
+warcraft 	= blizzard.games.create name: "Warcraft", 			year:1994
+warcraft2 	= blizzard.games.create name: "Warcraft II", 		year:1995
+diablo 		= blizzard.games.create name: "Diablo", 			year:1996
+starcraft 	= blizzard.games.create name: "Starcraft", 			year:1998
+diablo2 	= blizzard.games.create name: "Diablo II", 			year:2000
+warcraft3	= blizzard.games.create name: "Warcraft III", 		year:2002
+wow 		= blizzard.games.create name: "World of Warcraft", 	year:2004
+starcraft2	= blizzard.games.create name: "Starcraft II", 		year:2010
+diablo3 	= blizzard.games.create name: "Diablo III", 		year:2012
 
-populous 			= bullfrog.games.create name: "Populous"
-populous2 			= bullfrog.games.create name: "Populous II"
-theme_park 			= bullfrog.games.create name: "Theme Park"
-dungeon_keeper 		= bullfrog.games.create name: "Dungeon Keeper"
-theme_hospital 		= bullfrog.games.create name: "Theme Hospital"
-populous_beginning 	= bullfrog.games.create name: "Populous: The Beginning"
-dungeon_keeper2 	= bullfrog.games.create name: "Dungeon Keeper II"
+populous 			= bullfrog.games.create name: "Populous", 				year:1989
+populous2 			= bullfrog.games.create name: "Populous II", 			year:1991
+theme_park 			= bullfrog.games.create name: "Theme Park", 			year:1994
+dungeon_keeper 		= bullfrog.games.create name: "Dungeon Keeper", 		year:1997
+theme_hospital 		= bullfrog.games.create name: "Theme Hospital", 		year:1997
+populous_beginning 	= bullfrog.games.create name: "Populous: The Beginning",year:1998
+dungeon_keeper2 	= bullfrog.games.create name: "Dungeon Keeper II", 		year:1999
 
-warcraft.ratings.create score: 5, user_id: pauli.[:id]
+warcraft.ratings.create 	score: 5, user_id: pauli[:id]
+warcraft2.ratings.create 	score: 6, user_id: pauli[:id]
+warcraft3.ratings.create    score: 7, user_id: pauli[:id]
+wow.ratings.create 			score: 9, user_id: pauli[:id]
+starcraft2.ratings.create 	score:10, user_id: pauli[:id]
+diablo3.ratings.create 		score: 7, user_id: pauli[:id]
+
