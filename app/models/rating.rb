14d60a9b554d2c60dac5eefc7e8585cd9d90239f
Pauli Perälä <perala.pauli@gmail.com>
2013-09-23 01:12:09 +0300

    rating requires user

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,8 +2,11 @@ class Rating < ActiveRecord::Base
 	belongs_to :game
 	belongs_to :user
 
-	validates :score, numericality:{:in => 1..10}, format:{with:/\A\d*(\.\d)?\z/, message:"Must be a number with max 1 digit"} 
-	validates :game, uniqueness: {scope: :user, messages:"You cannot rate same game twice"}
+	validates :score, 	numericality:{:in => 1..10}, format:{with:/\A\d*(\.\d)?\z/, message:"Must be a number with max 1 digit"} ,
+						presence:true
+	validates :game, 	uniqueness: {scope: :user, message:"You cannot rate same game twice"}, 
+						presence:true
+	validates :user, 	presence:{message:"You have to be logged to do that"}
 	
 	def to_s
 		"#{game}: #{score}"
