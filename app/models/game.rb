3ef7abdb635533ad5a926d32e515a6c95058fd0b
Pauli Perälä <perala.pauli@gmail.com>
2013-09-22 17:01:17 +0300

    added relations between users and games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ActiveRecord::Base
 	belongs_to :publisher
 	has_many :ratings
+	has_many :raters, through: :ratings, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :ratings
+  has_many :games, through: :ratings
   validates :name, 	presence: true, 
   					uniqueness: true
   has_secure_password
