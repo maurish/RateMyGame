54a4d0f102511941d1f0ab2103af0c6dd61c7c84
Pauli Perälä <perala.pauli@gmail.com>
2013-09-22 13:57:34 +0300

    fixed a logging in bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   before_action :set_user, only: [:create]
 
   def create
-  	if @user
+  	if (not @user.nil?) and @user.authenticate user_params[:password]
   		session[:user_id] = @user.id
-  		redirect_to user_path @user, notice: "Logged in! Welcome back #{@user.name}"
+  		redirect_to user_path(@user), notice: "Logged in! Welcome back #{@user.name}"
   	else
   		redirect_to :back, alert: "Login was unsuccesful"
   	end
@@ -17,8 +17,7 @@ class SessionsController < ApplicationController
 
   private 
   def set_user 
-  	@user = User.find_by_name user_params[:name] 
-    (not @user.nil?) and @user.authenticate user_params[:password]
+  	@user = User.find_by_name user_params[:name]
   end
 
   def user_params
